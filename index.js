997017f0f80cd38f24a71375fcb932c566f2a303
July <majuliabonite@gmail.com>
2024-09-26 14:54:52 +0800

    Debug operators and clear functions
    Add working display

--- index.js
+++ index.js
@@ -58,6 +58,7 @@ function operate () {
         result = add ([firstVariable, secondVariable]);
         firstVariable = result;
         console.log ('firstVariableResult Add', firstVariable);
+        secondVariable = 0;
         return firstVariable; 
     }
 
@@ -76,6 +77,9 @@ function operate () {
     }
 
     if (operator === "/") {
+        if (i === 0) {
+            secondVariable = 1;
+        }
         result = divide ([firstVariable, secondVariable]);
         firstVariable = result;
         console.log ('firstVariableResult Divide', firstVariable);
@@ -88,71 +92,112 @@ function operate () {
         secondVariable = 0;
         result = 0;
         i = 0;
+        operator = "";
+        display.textContent = 0;
         console.log("Calculator reset.")
     }
 }
 
 
-// Event listeners for the buttons
+
+
+// Event listeners for the numeric buttons
 const btnOne = document.getElementById("1");
-    btnOne.addEventListener ("click", () => displayValue.push(1));
+    btnOne.addEventListener ("click", () => {
+        displayValue.push(1)
+        updateDisplay()});
 const btnTwo = document.getElementById("2");
-    btnTwo.addEventListener ("click", () => displayValue.push(2));
+    btnTwo.addEventListener ("click", () => {
+        displayValue.push(2)
+        updateDisplay()});
 const btnThree = document.getElementById("3");
-    btnThree.addEventListener ("click", () => displayValue.push(3));
+    btnThree.addEventListener ("click", () => {
+        displayValue.push(3)
+        updateDisplay()});
 const btnFour = document.getElementById("4");
-    btnFour.addEventListener ("click", () => displayValue.push(4));
+    btnFour.addEventListener ("click", () => {
+        displayValue.push(4)
+        updateDisplay()});
 const btnFive = document.getElementById("5");
-    btnFive.addEventListener ("click", () => displayValue.push(5));
+    btnFive.addEventListener ("click", () => {
+        displayValue.push(5)
+        updateDisplay()});
 const btnSix = document.getElementById("6");
-    btnSix.addEventListener ("click", () => displayValue.push(6));    
+    btnSix.addEventListener ("click", () => {
+        displayValue.push(6)
+        updateDisplay()});    
 const btnSeven = document.getElementById("7");
-    btnSeven.addEventListener ("click", () => displayValue.push(7));
+    btnSeven.addEventListener ("click", () => {
+        displayValue.push(7)
+        updateDisplay()});
 const btnEight = document.getElementById("8");
-    btnEight.addEventListener ("click", () => displayValue.push(8));
+    btnEight.addEventListener ("click", () => {
+        displayValue.push(8)
+        updateDisplay()});
 const btnNine = document.getElementById("9");
-    btnNine.addEventListener ("click", () => displayValue.push(9));
+    btnNine.addEventListener ("click", () => {
+        displayValue.push(9)
+        updateDisplay()});
 const btnZero = document.getElementById("0");
-    btnZero.addEventListener ("click", () => displayValue.push(0));
+    btnZero.addEventListener ("click", () => {
+        displayValue.push(0)
+        updateDisplay()});
 const btnDecimal = document.getElementById("decimal");
-    btnDecimal.addEventListener ("click", () => displayValue.push(Number('.')));
+    btnDecimal.addEventListener ("click", () => {
+        displayValue.push('.');
+        updateDisplay()});
 
-
+// Event listeners for the operators
 const btnAdd = document.getElementById("+");
     btnAdd.addEventListener ("click", () => {
+        updateResultFirst();
         operator = "+";
         assignValues();
         i = 1;
         operate();
+        updateDisplayResult();
     });
 
 const btnSubtract = document.getElementById("-");
     btnSubtract.addEventListener ("click", () => {
+        updateResultFirst();
         operator = "-";
         assignValues();
         i = 1;
         operate();
+        updateDisplayResult();
     });
 
 const btnDivide = document.getElementById("/");
     btnDivide.addEventListener ("click", () => {
+        updateResultFirst();
         operator = "/";
+        secondVariable = 1;
         assignValues();
+        operate();
         i = 1;
+        updateDisplayResult();
     });
 
 const btnMultiply = document.getElementById("*");
     btnMultiply.addEventListener ("click", () => {
+        updateResultFirst();
         operator = "*";
-        assignValues();
-        i = 1;
+        secondVariable = 1;
+        assignValues ();
+        operate ();
+        i=1;
+        updateDisplayResult();
+        if (displayValue.length > 0){
+            operate ();
+            assignValues();
+        }
     });
 
 const btnClear = document.getElementById("clear");
     btnClear.addEventListener ("click",() => {
         operator = "clear";
         operate();
-        i = 1;
     });
 
 const btnEqual = document.getElementById("=");
@@ -161,5 +206,26 @@ const btnEqual = document.getElementById("=");
         operate();
         i = 1;
         secondVariable = 0;
+        updateDisplayResult();
         console.log ("Result", firstVariable);
     })
+
+
+// Event listeners for the display
+const display = document.getElementById("display");
+
+function updateDisplay () {
+    display.textContent = displayValue.join('');
+}
+
+function updateDisplayResult () {
+    display.textContent = firstVariable;
+}
+
+function updateResultFirst () {
+        assignValues();
+        operate();
+        i = 1;
+        secondVariable = 0;
+        updateDisplayResult();
+}
